5cb1519b81bb787b30a297b1c0bac507cdff2fe7
Shreyash2003 <104262663+Shreyash2003@users.noreply.github.com>
2022-05-12 21:58:39 +0530

    Add files via upload

---
+++ Assignment solution.py
@@ -0,0 +1,54 @@
+C:\Users\shrey\PycharmProjects\pythonProject37\venv\Scripts\python.exe C:/Users/shrey/PycharmProjects/pythonProject37/main.py
+Q1
+Enter a number  68
+Binary equivalent of 68 is  1000100
+Q2
+Enter expression:  56*98-45/5+15
+56*98-45/5+15 = 5494.0
+Q3
+Enter an integer  14
+a) 464
+b) 91.0
+enter radius  8
+c) 804.247719318987
+Enter angle 1: 60
+Enter angle 1: 30
+d) 3.8815429668840293
+Enter x coordinate of first point: 34
+Enter y coordinate of first point: 20
+Enter x coordinate of second point: 12
+Enter y coordinate of second point: 30
+e) -0.45454545454545453
+Q4
+4.(a)
+0
+1
+2
+3
+4
+4.(b)
+3
+4
+5
+6
+7
+8
+9
+4.(c)
+4
+7
+10
+4.(d)
+15
+13
+11
+9
+7
+4.(e)
+Q5
+Enter number of carbon atoms in molecule: 2
+Enter number of hydrogen atoms in molecule: 6
+Enter number of oxygen atoms in molecule: 3
+The molecular weight of the compound is 78.06724
+
+Process finished with exit code 0
